06cd5656ec8335abe12c53b7e41fe912604a220d
Bruce Chen <brucechen1206personal@gmail.com>
2025-08-06 00:40:16 +0800

    allow icmp

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -66,6 +66,14 @@ resource "aws_security_group" "public" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "Allow ICMP"
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
